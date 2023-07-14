6813ee7b6225915fac2c4b41951d934a79dc7b63
Gregory Perez <Gregory10183@gmail.com>
2023-07-14 13:48:04 -0400

    url fetch actualizada

--- services/productos.js
+++ services/productos.js
@@ -1,5 +1,5 @@
 export default async function productos() {
-  return await fetch("http://localhost:4000/productos")
+  return await fetch("https://alurageekapi.gregorypf.site/productos")
     .then((res) => res.json())
     .then((res) => res)
     .catch((err) => console.log(err));
