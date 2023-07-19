4947d0fa37d9c4f569c4d6bfc43a8b43a28337cd
Gregory Perez <Gregory10183@gmail.com>
2023-07-19 17:10:51 -0400

    boton ver consolas funcional

--- scripts/searchProducts.js
+++ scripts/searchProducts.js
@@ -8,18 +8,19 @@ const productData = await productos();
 
 const urlParams = new URLSearchParams(window.location.search);
 const searchedProducts = urlParams.get("searchString");
+const searchedCategory = urlParams.get("searchCategory");
 
 const found = productData.filter(
   (product) =>
-    product.name.toLowerCase().includes(searchedProducts.toLowerCase()) ||
-    product.category.toLowerCase().includes(searchedProducts.toLowerCase())
+    product.name.toLowerCase().includes(searchedProducts?.toLowerCase()) ||
+    product.category.toLowerCase().includes(searchedCategory?.toLowerCase())
 );
 
 function capitalize(s) {
   return s[0].toUpperCase() + s.slice(1);
 }
 
-productsTitle.innerHTML = capitalize(searchedProducts);
+productsTitle.innerHTML = capitalize(searchedProducts || searchedCategory);
 
 if (found) {
   found.forEach((product) => {
